b5a71149102b1180b3676cc0e0ac482a2bc91bc0
Cosmo <cosmo.lepton@gmail.com>
2017-06-01 12:25:03 -0700

    Update init_vim.sh

--- init_vim.sh
+++ init_vim.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 # @author CosmoX <cosmo.lepton@gmail.com>
 
-MARKER="-----> "
-vimrcFile="https://raw.githubusercontent.com/hackjutsu/Cosmo-Configs/master/.vimrc"
-colorSchemeFileHybrid="https://raw.githubusercontent.com/w0ng/vim-hybrid/master/colors/hybrid.vim"
-
-pushd .
-
+pushd . > /dev/null ;
 cd ~
 
+MARKER="-----> "
+vimrcFile="https://raw.githubusercontent.com/hackjutsu/Cosmo-Configs/master/.vimrc"
+
+# ---------- Color Schemes ----------
+colorscheme_hybrid="https://raw.githubusercontent.com/w0ng/vim-hybrid/master/colors/hybrid.vim"
+colorscheme_crayon="https://raw.githubusercontent.com/jansenfuller/crayon/master/colors/crayon.vim"
+colorscheme_happy_hacking="https://github.com/YorickPeterse/happy_hacking.vim/blob/master/colors/happy_hacking.vim"
+
+colorschemes=(
+    $colorscheme_hybrid
+    $colorscheme_crayon
+    $colorscheme_happy_hacking
+)
+
+
+# ---------- Decide the host OS ----------
 os="`uname`"
 case "$OSTYPE" in
   solaris*) os="SOLARIS" ;;
@@ -19,10 +30,12 @@ case "$OSTYPE" in
   *)        os="unknown: $OSTYPE" ;;
 esac
 
+
+# ---------- Download scaffold tools ----------
 if [ $os == "OSX" ]; then
     which -s brew > /dev/null
     if [ $? == 0 ]; then
-        echo $MARKER "Homebrew Exists"
+        echo $MARKER "Homebrew exists"
     else
         echo $MARKER "Installing Homebrew..."
         /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
@@ -30,7 +43,7 @@ if [ $os == "OSX" ]; then
 
     which -s curl > /dev/null
     if [ $? == 0 ]; then
-        echo $MARKER "curl Exists"
+        echo $MARKER "curl exists"
     else
         echo $MARKER "Installing curl..."
         brew install curl
@@ -38,42 +51,70 @@ if [ $os == "OSX" ]; then
 
     which -s vim > /dev/null
     if [ $? == 0 ]; then
-        echo $MARKER "vim Exists"
+        echo $MARKER "vim exists"
     else
     echo $MARKER "Installing vim..."
         brew install vim
     fi
 
+    which -s ag > /dev/null
+    if [ $? == 0 ]; then
+        echo $MARKER "The Silver Searcher exists"
+    else
+    echo $MARKER "Installing the silver searcher..."
+        brew install the_silver_searcher
+    fi
+
 elif [ $os == "LINUX" ]; then
-    echo "apt-get install curl"
-    echo "apt-get install vim"
+    # Assuming we are in Ubuntu...
+    apt-get install curl
+    apt-get install vim
+    apt-get install silversearcher-ag
 else
     echo "Platform not supported."
 fi
 
-# Create ~/.vim if not exist
+
+# ---------- Create ~/.vim if not exist ----------
 if [ ! -d "$HOME/.vim" ]; then
     echo $MARKER "Creating the .vim directory..."
     mkdir ~/.vim
 fi
 
-# Download the color scheme
-echo $MARKER "Downloading the color scheme..."
-curl -fLo ~/.vim/colors/hybrid.vim --create-dirs $colorSchemeFileHybrid
 
-# Sync .vimrc
+# ---------- Download color schemes ----------
+pushd . > /dev/null
+if [ ! -d "$HOME/.vim/colors" ]; then
+    echo $MARKER "Creating the ~/.vim/colors directory..."
+    mkdir -p ~/.vim/colors
+fi
+cd ~/.vim/colors
+
+echo $MARKER "Downloading the color schemes..."
+for colorscheme in "${colorschemes[@]}"; do
+    echo $MARKER "Downloading $colorscheme"
+	curl -O $colorscheme
+done
+popd > /dev/null
+
+
+# ---------- Download .vimrc ----------
 if [ -f "$HOME/.vimrc" ]; then
+    echo $MARKER "Backing up the old .vimrc as .vimrc-backup..."
     mv ~/.vimrc ~/.vimrc-backup
 fi
 echo $MARKER "Downloading .vimrc file..."
 curl -O $vimrcFile
 
-# Install vim-plug
-echo $MARKER "Installing vim-plug..."
+
+# ---------- Install plugin manager: vim-plug ----------
+echo $MARKER "Installing plugin manager: vim-plug..."
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# install vim plugins
+
+# ---------- Install vim plugins ----------
 echo $MARKER "Installing vim plugins..."
 vim +PlugInstall +qall
 
-popd
+
+popd > /dev/null
